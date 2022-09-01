0602f730da9efb77acd09a98fc6cd30b5e8e830c
Major2571 <caroline.tacats62@gmail.com>
2022-08-31 22:35:59 -0300

    att files

---
+++ 2Módulo/DSI/Aula 06/LacosDeRepetiçoes/ForNumerosImparesPares.java
@@ -0,0 +1,20 @@
+package LacosDeRepetiçoes;
+
+public class ForNumerosImparesPares {
+
+    public static void main(String[] args) {
+
+        int i;
+
+        for (i = 0; i <= 100; i++) {
+
+            if (i % 2 == 0) {
+                System.out.println(i + " (  número par  )");
+            } else {
+                System.out.println(i + " ( número impar )");
+            }
+
+        }
+
+    }
+}
---
+++ 2Módulo/DSI/Aula 06/LacosDeRepetiçoes/Tabuada.java
@@ -0,0 +1,18 @@
+
+package LacosDeRepetiçoes;
+
+public class Tabuada {
+
+    public static void main(String[] args) {
+
+        int i, resultado, numero=6;
+
+        for (i = 1; i <= 10; i++) {
+
+            resultado = numero * i; // i é meu contador
+            System.out.println(numero + " * " + i + " = " + resultado);
+
+        }
+
+    }
+}
