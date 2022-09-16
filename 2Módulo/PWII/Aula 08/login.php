d9aa9cd05b7803b8aba5e59f755c852017695944
Major2571 <caroline.tacats62@gmail.com>
2022-09-16 20:40:36 -0300

    Aula 8

---
+++ 2Módulo/PWII/Aula 08/login.php
@@ -0,0 +1,18 @@
+<section>
+        <form action="">
+
+            <div>
+                <input type="text" placeholder="Usuario">
+            </div>
+
+            <div>
+                <input type="text" placeholder="Senha">
+            </div>
+
+            <div>
+                <input type="submit" placeholder="Salvar" class="btn btn-info">
+            </div>
+
+        </form>
+
+    </section>
---
+++ 2Módulo/PWII/Aula 08/principal.php
@@ -0,0 +1,6 @@
+<section>
+
+    <h1> Total de Palavras: </h1>
+    <h1> Total de Usuários: </h1>
+
+</section>
