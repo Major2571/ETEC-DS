cf42cd7f8d5285d7ddeea07b981d2648de52dbc8
Major2571 <caroline.tacats62@gmail.com>
2022-09-19 11:35:03 -0300

    Add Atividade Glossario

---
+++ 2Módulo/PWII/Glossario/login.php
@@ -0,0 +1,22 @@
+<?php include("templates/head.php"); ?>
+
+<section class="login">
+        <form action="">
+
+            <div>
+                <input type="text" placeholder="Usuario">
+            </div>
+
+            <div>
+                <input type="text" placeholder="Senha">
+            </div>
+
+            <div>
+                <input type="submit" placeholder="Salvar">
+            </div>
+
+        </form>
+
+</section>
+
+    <?php include("templates/footer.php"); ?>
