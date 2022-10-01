01b6c2a5b2ba8c079818f867a583f8a772fb5497
Major2571 <caroline.tacats62@gmail.com>
2022-09-30 23:34:25 -0300

    aula10 banco de dados ( excluir e inserir pelo php )

---
+++ 2Módulo/PWII/Glossario/Action_ExcluirPalavra.php
@@ -0,0 +1,20 @@
+<?php
+
+$idPalavra = $_GET['id'];
+
+
+include("BD_Conexao.php");
+    try {
+
+        $stmt = $pdo->prepare("delete from tbPalavra where idPalavra = '$idPalavra';");
+
+        $stmt->execute();
+
+        $pdo = null;
+
+        header("Location:BD_ConsultaPalavra.php");
+
+    }
+    catch (PDOException $e) {
+        echo "Error: " . $e->getMessage();
+    }
