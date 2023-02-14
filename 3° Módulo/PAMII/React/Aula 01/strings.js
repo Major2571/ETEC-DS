588f59f36b933180ac2f9984fd38c332d1e7185a
Major2571 <caroline.tacats62@gmail.com>
2023-02-13 22:26:40 -0300

    feat: aula01 js

---
+++ 3° Módulo/PAMII/React/Aula 01/strings.js
@@ -0,0 +1,14 @@
+const something = 'Cod3r';
+
+console.log(something.charAt(4));
+console.log(something.charAt(5));
+
+console.log(something.charCodeAt(3));
+console.log(something.indexOf('o'));
+
+console.log(something.substring(1));
+
+console.log(something.replace(3, 'e'));
+console.log(something.replace(/\d/, 'e')); //regex
+
+console.log('Ana, Maria, Pedro'.split(','));
---
+++ 3° Módulo/PAMII/React/Aula 01/tipagem.js
@@ -0,0 +1,19 @@
+let adjetivo = 'legal';
+console.log(adjetivo);
+console.log(typeof adjetivo);
+
+let pi = 3.14;
+console.log(pi);
+console.log(typeof pi);
+
+let frutas = [ 'maça', 'banana', 'kiwi'];
+console.log(frutas);
+console.log(typeof frutas);
+
+let alimento = {
+    nome: 'alface',
+    tipo: 'vegetal',
+    preco: 2.0
+}
+console.log(alimento);
+console.log(typeof alimento);
