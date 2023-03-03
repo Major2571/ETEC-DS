9f61e5b1793201412fa1cb4ffeb5cac4ed0d96d5
Major2571 <caroline.tacats62@gmail.com>
2023-03-02 22:24:59 -0300

    feat: routes, model, controller ( MVC )

---
+++ 3° Módulo/PWIII/Aula_4/routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('index');
+});
+
+Route::get('/laboratorio', function () {
+    return view('laboratorio');
+});
+
+Route::get('/contato', function () {
+    return view('contato');
+});
+
+Route::get('/quemSomos', function () {
+    return view('quemSomos');
+});
+
+Route::get('/reclamacoes', function () {
+    return view('reclamacoes');
+});
