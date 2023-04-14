69668d8cb8bb1f4f8e9c07f1b179a20703154399
Major2571 <caroline.tacats62@gmail.com>
2023-04-13 22:17:32 -0300

    feat: filters and count

---
+++ 3° Módulo/PWIII/Aula_10/app/Http/Controllers/ReclamacoesController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Reclamacoes;
+use Illuminate\Http\Request;
+
+class ReclamacoesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+
+        $searchNumber = request('searchNumber');
+
+        $searchDate = request('searchDate');
+
+        if($searchNumber){
+            $reclamacoes = Reclamacoes::where('idLab', '=', $searchNumber)->get();
+        } 
+        
+        if($searchDate) {
+            $reclamacoes = Reclamacoes::where('dtCriacao', '=', $searchDate)->get();
+        }
+
+        else {
+            $reclamacoes = Reclamacoes::orderBy('dtCriacao')->get();
+        }
+        
+
+        return view('reclamacoes', compact('reclamacoes', 'searchNumber'));
+    }
+
+    // Slect Count
+    public static function totalDeReclamacoes(){
+        $reclamacoes = new Reclamacoes();        
+        $total =  $reclamacoes::all()->count();
+        return $total;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $reclamacoes = new Reclamacoes();
+
+        $reclamacoes -> idLab = $request -> txLab;
+        $reclamacoes -> pc = $request -> txPc;
+        $reclamacoes -> titulo = $request -> txTitulo;
+        $reclamacoes -> descricao = $request -> txDescricao;
+
+        $reclamacoes->save();
+
+        return redirect('/reclamacoes');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Reclamacoes $reclamacoes)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Reclamacoes $reclamacoes)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Reclamacoes $reclamacoes)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy($idRec)
+    {
+        $reclamacoes = new Reclamacoes();
+        $reclamacoes->where('idRec', $idRec)->delete();
+        return redirect('/reclamacoes');
+    }
+}
---
+++ 3° Módulo/PWIII/Aula_10/app/Reclamacoes.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Reclamacoes extends Model
+{
+    // use HasFactory;
+    protected $table = 'tbReclamacao';
+
+    protected $fillable = [ 'idRec', 'idLab', 'pc', 'titulo', 'descricao', 'dtCriacao'];
+
+    public $timestamps = false;
+}
