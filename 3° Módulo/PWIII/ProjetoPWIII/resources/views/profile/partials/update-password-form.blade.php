db8977d2e6aea05b049553d6dc48d99cc86ec641
Major2571 <caroline.tacats62@gmail.com>
2023-05-22 22:20:47 -0300

    style: password form

--- 3° Módulo/PWIII/ProjetoPWIII/resources/views/profile/partials/update-password-form.blade.php
+++ 3° Módulo/PWIII/ProjetoPWIII/resources/views/profile/partials/update-password-form.blade.php
@@ -1,10 +1,10 @@
 <section>
     <header>
-        <h2 class="text-lg font-medium text-gray-900">
+        <h2 class="text-lg font-medium text-white">
             {{ __('Update Password') }}
         </h2>
 
-        <p class="mt-1 text-sm text-gray-600">
+        <p class="mt-1 text-sm text-white">
             {{ __('Ensure your account is using a long, random password to stay secure.') }}
         </p>
     </header>
@@ -13,19 +13,23 @@
         @csrf
         @method('put')
 
-        <div>
-            <x-input-label for="current_password" :value="__('Current Password')" />
-            <x-text-input id="current_password" name="current_password" type="password" class="mt-1 block w-full" autocomplete="current-password" />
-            <x-input-error :messages="$errors->updatePassword->get('current_password')" class="mt-2" />
+        <div class="relative z-0 w-full my-6 group">
+            <input type="password" name="current-password" id="current-password" class="block py-2.5 px-0 w-full text-black bg-transparent border-0 border-b-2 border-gray-300 appearance-none  focus:outline-none focus:ring-0 focus:border-[#DE3F4D] peer" placeholder=" " required autofocus autocomplete="current-password"/>
+            <label for="email" class="peer-focus:font-medium absolute text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-2.5 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#DE3F4D] peer-focus:dark:text-[#DE3F4D] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
+                Senha
+            </label>
         </div>
 
-        <div>
-            <x-input-label for="password" :value="__('New Password')" />
+        <div  class="relative z-0 w-full my-6 group">
+            <label for="password" :value="__('New Password')" 
+            type="password" name="password" id="password" class="block py-2.5 px-0 w-full text-black bg-transparent border-0 border-b-2 border-gray-300 appearance-none  focus:outline-none focus:ring-0 focus:border-[#DE3F4D] peer" placeholder=" " required autofocus
+            > Nova Senha </label>
             <x-text-input id="password" name="password" type="password" class="mt-1 block w-full" autocomplete="new-password" />
+
             <x-input-error :messages="$errors->updatePassword->get('password')" class="mt-2" />
         </div>
 
-        <div>
+        <div  class="relative z-0 w-full my-6 group">
             <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
             <x-text-input id="password_confirmation" name="password_confirmation" type="password" class="mt-1 block w-full" autocomplete="new-password" />
             <x-input-error :messages="$errors->updatePassword->get('password_confirmation')" class="mt-2" />
@@ -40,7 +44,7 @@
                     x-show="show"
                     x-transition
                     x-init="setTimeout(() => show = false, 2000)"
-                    class="text-sm text-gray-600"
+                    class="text-sm text-white"
                 >{{ __('Saved.') }}</p>
             @endif
         </div>
